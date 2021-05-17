8a78bafbdd13af6752202f6e7fe019d1d6fed5aa
Alexandr <alekspetrofint@gmail.com>
2021-05-17 13:45:48 +0300

    create logger

--- src/app.ts
+++ src/app.ts
@@ -4,20 +4,23 @@ const analyticsRoutes = require('./routes/analytics')
 const categoryRoutes = require('./routes/category')
 const orderRoutes = require('./routes/order')
 const positionRoutes = require('./routes/position')
-import morgan = require('morgan');
-//http://localhost:5000/api/auth/login
+const logger = require('./logger/logger')
+const morgan = require('morgan')
 require("dotenv").config()
 declare let process: {
   env: {
-    PORT: number; 
+    PORT: number;
     HOST: string;
   };
 };
+
+//logger.info('Hello again distributed logs');
 
 let app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 //app.use(require('morgan')('dev'))
+app.use(require("morgan")("combined", { stream: logger.stream }));  //added here
 app.use(require('cors')())
 
 app.use('/api/auth', authRoutes)
