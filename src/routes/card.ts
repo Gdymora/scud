f0b61e9a13f96d89cc8463dc4f3300a73861218b
Alexandr <alekspetrofint@gmail.com>
2021-09-22 12:15:23 +0300

    fix passport Model

--- src/routes/card.ts
+++ src/routes/card.ts
@@ -3,7 +3,7 @@ const controller = require("../controllers/card");
 const passport = require('passport')
 const router = express.Router()
 
-router.get('/:id', controller.getAll)
+router.get('/', controller.getAll)
 router.get('/:id', controller.getById)
 router.post('/', passport.authenticate('jwt', { session: false }), controller.create)
 router.patch('/:id', passport.authenticate('jwt', { session: false }), controller.update)
